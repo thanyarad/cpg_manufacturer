d7263267b60b013ded834158128828e7e5e4e8e1
gladwin2802 <ajgladwin2802@gmail.com>
2025-11-25 09:35:20 +0530

    Refactor consumer orders transformations to use temporary views for improved data handling. Added validation for consumer IDs and streamlined the creation of the main materialized view.

--- manufacturer/src/consumer_orders/consumer_orders_silver/transformations/silver.py
+++ manufacturer/src/consumer_orders/consumer_orders_silver/transformations/silver.py
@@ -5,44 +5,53 @@ catalog="dev"
 from_schema="01_bronze"
 to_schema="02_silver"
 
-@dp.materialized_view(name=f"{catalog}.{to_schema}.consumer_orders_mv")
-@dp.expect_or_drop("valid_order", "order_id IS NOT NULL")
-@dp.expect_or_drop("valid_order_item", "order_item_id IS NOT NULL")
-def consumer_orders_silver_mv():
-    orders_df = spark.read.table(f"{catalog}.{from_schema}.consumer_order_mv")
-    items_df = spark.read.table(f"{catalog}.{from_schema}.consumer_order_items_mv")
-    
+@dp.expect_all_or_drop({
+    "valid_order": "order_id IS NOT NULL",
+    "valid_consumer": "consumer_id IS NOT NULL"
+})
+@dp.temporary_view(name=f"consumer_order_temp")
+def consumer_order_temp():
+    df = spark.read.table(f"{catalog}.{from_schema}.consumer_order_mv")
     string_cols = ["order_status","currency","payment_method","channel","billing_address","shipping_address"]
     for c in string_cols:
-        orders_df = orders_df.withColumn(c, trim(col(c)))
-    
-    orders_df = orders_df.withColumn("order_date", to_date(col("order_date"), "yyyy-MM-dd"))
-    orders_df = orders_df.withColumn(
+        df = df.withColumn(c, trim(col(c)))
+    df = df.withColumn("order_date", to_date(col("order_date"), "yyyy-MM-dd"))
+    df = df.withColumn(
         "total_amount",
         when(col("total_amount").isNotNull() & (col("total_amount") >= 0), col("total_amount"))
         .otherwise(None)
     )
-    
-    items_df = items_df.withColumn(
+    return df
+
+@dp.expect_all_or_drop({
+    "valid_order": "order_id IS NOT NULL",
+    "valid_order_item": "order_item_id IS NOT NULL"
+})
+@dp.temporary_view(name=f"consumer_order_item_temp")
+def consumer_order_item_temp():
+    df = spark.read.table(f"{catalog}.{from_schema}.consumer_order_items_mv")
+    df = df.withColumn(
         "quantity",
         when(col("quantity").isNotNull() & (col("quantity") > 0), col("quantity"))
         .otherwise(None)
     )
-    items_df = items_df.withColumn(
+    df = df.withColumn(
         "unit_price",
         when(col("unit_price").isNotNull() & (col("unit_price") >= 0), col("unit_price"))
         .otherwise(None)
     )
-    items_df = items_df.withColumn(
+    df = df.withColumn(
         "total_price",
         when(col("total_price").isNotNull() & (col("total_price") >= 0), col("total_price"))
         .otherwise(None)
     )
-    
-    orders_df.createOrReplaceTempView("orders_temp")
-    items_df.createOrReplaceTempView("items_temp")
-    
-    joined_df = spark.sql("""
+    return df
+
+@dp.materialized_view(name=f"{catalog}.{to_schema}.consumer_orders_mv")
+def consumer_orders_silver_mv():
+    consumer_order_temp()
+    consumer_order_item_temp()
+    joined_df = spark.sql(f"""
         SELECT 
             i.order_item_id,
             i.order_id,
@@ -59,10 +68,9 @@ def consumer_orders_silver_mv():
             o.channel,
             o.billing_address,
             o.shipping_address
-        FROM items_temp i
-        INNER JOIN orders_temp o
+        FROM consumer_order_item_temp i
+        LEFT JOIN consumer_order_temp o
         ON i.order_id = o.order_id
     """)
-    
     return joined_df
 
